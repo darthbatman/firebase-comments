94cc66b54c866e4496b62a5464eb094bd4104298
Rishi Masand <rishimasand@Rishis-iMac.home>
2016-08-15 22:08:35 -0400

    posting and receiving comments working

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var config = {
+	apiKey: "AIzaSyD3uA_mMoDqG9C3bEMKLjBJNt6V5au8U8k",
+	authDomain: "fir-comments-3fe01.firebaseapp.com",
+	databaseURL: "https://fir-comments-3fe01.firebaseio.com",
+	storageBucket: "fir-comments-3fe01.appspot.com",
+};
+firebase.initializeApp(config);
+var dbRef = firebase.database().ref().child('comments');
+$('#submit').on('click', function(){
+	var d = new Date();
+	dbRef.push({
+		username: $('#username_field').val(),
+		comment_body: $('#comment_body_field').val(),
+		date: d.getTime()
+	});
+});
+dbRef.on('child_added', function(data){
+	$('#comment-area').after('<div class="comment"><p class="username">' + data.val().username + '</p><p class="comment_body">' + data.val().comment_body + '</p></div>');
+});
